30598d72e474f232fa2754cc4a9b2eb7936b0e58
Minkaron <aaron.tucaz@gmail.com>
2020-08-01 17:53:02 -0700

    Adding styling and landing page

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const port = 3000;
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+
+
+app.listen(port, () => {
+    console.log(`Express is listening on port:${port}`);
+})
